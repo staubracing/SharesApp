1333d78bfe369265566cd3abfe81787112db72de
StaubRacing <staubracing@gmail.com>
2023-12-12 06:31:29 -0600

    Add CustomInput component

---
+++ components/common/input.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, TextInput } from "react-native";
+
+type InputProps = {
+    value: string;
+    onChangeText: (text: string) => void;
+    placeholder: string;
+    keyboardType: "default" | "numeric";
+};
+
+export default function CustomInput({ value, onChangeText, placeholder, keyboardType }: InputProps) {
+    return (
+        <TextInput
+            keyboardType={keyboardType}
+            value={value}
+            onChangeText={onChangeText}
+            placeholder={placeholder}
+            style={styles.input}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: "white",
+        padding: 10,
+        borderRadius: 5,
+        marginVertical: 10,
+    },
+});
+
