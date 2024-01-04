24204a1350f538f6f91ea17b1e1bb6b00e7e1d35
StaubRacing <staubracing@gmail.com>
2024-01-04 06:17:59 -0600

    removed comment

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ import { SharesComponent } from './components/SpecificComponents';
 import LoginForm from './screens/Login';
 
 import { Amplify } from 'aws-amplify';
-import awsConfig from './services/aws-exports'; // if default export
+import awsConfig from './services/aws-exports'; 
 
 Amplify.configure(awsConfig);
 
